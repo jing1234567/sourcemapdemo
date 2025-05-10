1604d232bb35c1cb6ddb3af2fb391cf03a99fe88
jing1234567 <1599741778@qq.com>
2025-05-10 21:52:19 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+let testNum = null;
+if (testNum.length > 0) {
+  console.log('testNum is not null');
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
